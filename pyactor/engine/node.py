4ba4ebe54d3ab98a605644b4bfd8ff6fee5de3ef
lmarek <marek.lucas@gmail.com>
2018-09-23 01:01:39 +0200

    using non-async python again #todo use asyncio here

--- pyactor/engine/node.py
+++ pyactor/engine/node.py
@@ -51,10 +51,9 @@ class Node:
         self._external_queue_in.put(message)
 
     def start(self):
-        loop = asyncio.new_event_loop()
-        loop.run_until_complete(self.__start())
+        self.__start()
 
-    async def __start(self):
+    def __start(self):
         self._event_loop_thread.start()
         while True:
             internal_message_received = self._handle_internal_message()
